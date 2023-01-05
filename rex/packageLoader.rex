113d9560b2e0e5882f8dbd9c5a1b7a018daf310c
xiaokang00010 <zhouyongkang4321@gmail.com>
2023-01-05 22:26:47 +0800

    bugfix

--- rex/packageLoader.rex
+++ rex/packageLoader.rex
@@ -4,3 +4,4 @@ let fs = __module__.fs;
 let sqlite = __module__.sqlite;
 let net = __module__.net;
 let json = __module__.json;
+let time = __module__.time;
--- std/std.cpp
+++ std/std.cpp
@@ -5,9 +5,9 @@
 #include "std.hpp"
 
 void rexModInit(const managedPtr<environment> &env, const managedPtr<value> &mod) {
+    mod->members[L"time"] = managePtr(value{rexStd::time::getMethodsCxt()});
     mod->members[L"fs"] = managePtr(value{rexStd::fs::getMethodsCxt()});
     mod->members[L"sqlite"] = managePtr(value{rexStd::sqlite::getMethodsCxt()});
     mod->members[L"net"] = managePtr(value{rexStd::net::getMethodsCxt()});
     mod->members[L"json"] = managePtr(value{rexStd::json::getMethodsCxt()});
-    mod->members[L"time"] = managePtr(value{rexStd::time::getMethodsCxt()});
 }
