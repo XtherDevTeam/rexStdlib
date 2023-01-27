06234749baad77f271b2c2ba2234f46aacdfd926
xiaokang00010 <zhouyongkang4321@gmail.com>
2023-01-27 15:32:28 +0800

    remove test file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 
-#include "std/json.hpp"
-
 int main() {
-    std::string s = "{\n"
-    "  \"name\": \"std\",\n"
-    "  \"description\": \"reXscript Standard Library\",\n"
-    "  \"version\": \"0.1\",\n"
-    "  \"dependencies\": {}\n"
-    "}";
-    rex::value res{};
-    auto result = rexStd::json::loadObjectToJson(rex::string2wstring(s), res, 0);
     return 0;
 }
